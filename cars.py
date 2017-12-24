a4ec8cae843ef8507c64105d95f82ee57a722f07
Matthew Ung <matthew.ung814@gmail.com>
2017-12-23 23:40:37 -0800

    code is working

--- cars.py
+++ cars.py
@@ -1,17 +1,18 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import random 
 from matplotlib.animation import FuncAnimation
 
-length = 5 
-p = 0.3
+length = 40
+size = {10 : 2000, 
+        20 : 500,
+        40 : 100}
+p = 0.30
 
 fig = plt.figure(figsize=(7,7))
 ax = fig.add_axes([0,0,1,1], frameon=True)
 ax.set_xlim(0, length), ax.set_xticks([])
 ax.set_ylim(0, length), ax.set_yticks([])
 
-#cars = np.zeros(length, dtype=[('car', bool), ('color'
 
 x,y,colors = [],[],[]
 grid = [[None for _ in range(length)] for _ in range(length)]
@@ -20,39 +21,21 @@ scat = None
 
 def init():
     global scat
-    """
+
     for r in range(length):
         for c in range(length):
             if random.random() < p:
-                #color = 'red' if random.random() < 0.5 else 'blue'
-                color = 'red'
+                color = 'red' if random.random() < 0.5 else 'blue'
                 x.append(c+0.5)
                 y.append(r+0.5)
                 colors.append(color)
 
                 grid[r][c] = [color, False]
-    """
-    x = [0, 1, 1]
-    y = [0, 0, 1]
-    colors = ['red']*3
-    grid[0][0] = ['red', False]
-    grid[0][1] = ['red', False]
-    grid[1][1] = ['red', False]
 
-    scat = ax.scatter(x,y,marker='s', s=2000, color=colors)
-    print('done init!')
-    print(x)
-    print(y)
-    print(colors)
+    scat = ax.scatter(x,y,marker='s', s=size[length], color=colors)
 
 
 def update(fr):
-    print("attempting to update")
-    """
-    for i in range(length):
-        print(grid[i])
-        """
-
     for i in range(length):
         for j in range(length):
             if grid[i][j] != None:
@@ -61,7 +44,6 @@ def update(fr):
     def move_up(r, c):
         global x,y,colors
         new_r = r+1 if r+1 <length else 0
-        print('attempting to move', r,c, grid[r][c])
         
         if grid[r][c][1]:
             return
@@ -75,7 +57,6 @@ def update(fr):
             colors.append(grid[new_r][c][0])
 
         elif grid[new_r][c][0] is grid[r][c][0] and not grid[new_r][c][1]:
-            print('color:', grid[new_r][c][0], grid[r][c][0])
             grid[r][c][1] = True
             move_up(new_r, c)
             if grid[new_r][c]:
@@ -100,7 +81,6 @@ def update(fr):
 
     def move_right(r, c):
         global x,y,colors
-        print('attempting to move right', r,c, grid[r][c])
         new_c = c+1 if c+1 < length else 0
         
         if grid[r][c][1]:
@@ -116,7 +96,7 @@ def update(fr):
 
         elif grid[r][new_c][0] is grid[r][c][0] and not grid[r][new_c][1]:
             grid[r][c][1] = True
-            move_up(r, new_c)
+            move_right(r, new_c)
             if grid[r][new_c]:
                 grid[r][c][1] = True
                 x.append(c+0.5)
@@ -139,23 +119,34 @@ def update(fr):
 
     global x, y, colors 
     global turn
-    turn = 'blue' if turn is 'red' else 'red'
     x,y,colors = [],[],[]
+
     for r in range(length):
         for c in range(length):
             if grid[r][c] and grid[r][c][0] is 'blue':
-                continue
-                move_up(r,c)
+                if turn is 'blue':
+                    move_up(r,c)
+                else:
+                    x.append(c+0.5)
+                    y.append(r+0.5)
+                    colors.append('blue')
 
     for r in range(length):
         for c in range(length):
             if grid[r][c] and grid[r][c][0] is 'red':
-                move_right(r,c)
+                if turn is 'red':
+                    move_right(r,c)
+                else:
+                    x.append(c+0.5)
+                    y.append(r+0.5)
+                    colors.append('red')
 
     scat.set_color(colors)
     scat.set_offsets(list(zip(x,y)))
-    scat.set_sizes([2000]*len(x))
+    scat.set_sizes([size[length]]*len(x))
 
-animation = FuncAnimation(fig, update, interval=2000, init_func=init)
+    turn = 'blue' if turn is 'red' else 'red'
+
+animation = FuncAnimation(fig, update, interval=50, init_func=init)
 plt.show()
 
